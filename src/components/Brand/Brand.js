320340a45b97916ab8231d5dd469011495ed8c58
bruno rocca <bruno.rocca@gmail.com>
2024-03-18 16:09:17 -0700

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -10,8 +10,8 @@ const Brand = (props) => {
       role={'presentation'}
       onClick={() => navigate('/')}
     >
-      { <h4>SYDEY</h4> }
-      <svg
+      { <h4>INFINIUM</h4> }
+     /* <svg
         width={127}
         height={24}
         xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +41,7 @@ const Brand = (props) => {
           fill="currentColor"
           d="M121.59.48V1l2.4,1-4.87,10.72L114.39,3.2a5.11,5.11,0,0,0-1.67-2,4.47,4.47,0,0,0-2.65-.7h-4V1l1.6.48,6.56,13.12v7.36l-1.92.48v.48h9.12V22.4l-1.92-.48V14.24l5.6-12.32,2.56-1V.48Z"
         />
-      </svg>
+      </svg> */
     </div>
   );
 };
