00ab01ed58c68d4b2ad8aa4f61ed18521ddf686d
bruno rocca <bruno.rocca@gmail.com>
2024-03-18 08:33:59 -0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -48,13 +48,13 @@ const AboutPage = (props) => {
         <Container size={'large'} spacing={'min'}>
           <div className={styles.detailContainer} ref={historyRef}>
             <p>
-              Founded in 1860, INFINIUM is an innovative global brand with a
-              contemporary edge. We make timeless lithium batteries.
+              INFINIUM is an innovative global battery brand with a
+              contemporary edge. We make timeless lithium batteries that have specifications way above the rest..
             </p>
             <br />
             <br />
             <p>
-              We created some of the world's first LFP batteries and spent decades
+              We took over 30 years of  some of the world's first LFP batteries and spent decades
               perfecting the battery management systems that power our batteries.Today we are the only brand
               that integrates its battery management systems with its own customers.
             </p>
@@ -70,16 +70,7 @@ const AboutPage = (props) => {
             <h3>Our Values</h3>
             <div ref={valuesRef}>
               <p>
-                Sunspel produced some of the world's earliest T-shirts. In the
-                late 1800s the business made luxury tunics and undershirts from
-                lightweight Sea Island cotton for export to the Far East and
-                other warm climates. While these garments initially had silk
-                buttoned plackets, these were removed in the early 1900s and
-                replaced with simple bound necks to reduce manufacturing costs -
-                creating the T-shirt. We've supplied the world as the T-shirt
-                has evolved from underwear to outerwear, from symbol of youthful
-                rebellion to everyday wardrobe staple, and we've spent decades
-                refining its every last aspect.
+                INFINUM batteryes are the most reliable LFP batteries on the market. They are designed that way, and built by hand with maximum precision, then tested to insure that every battery exceeds its life.
               </p>
               <ol>
                 <li>Be an ecowear</li>
@@ -91,26 +82,13 @@ const AboutPage = (props) => {
             <h3>Sustainability</h3>
             <div id={'#sustainability'} ref={sustainabilityRef}>
               <p>
-                Our founder, Bruno Rocca, had both an eye for quality and a
-                desire to innovate. As well as using the finest fibres such as
-                Sea Island cotton, cashmere and silk, he invented his own
-                fabrics. Sunspel continues this commitment to innovation today
-                and our unique fabrics include: Q100 Sea Island cotton, Q82
-                Supima cotton, Q75 warp knit mesh cotton and Q14 warp knit
-                cellular cotton. The technology behind these fabrics remains
-                unchanged today and all Sunspel products use the finest cottons,
-                wools and fibres.
+                Our development team 
               </p>
               <p>
-                Made in Long Eaton, England and crafted from our luxurious long
-                staple Supima cotton for unparalleled softness, comfort and
-                durability, the Sunspel T-shirt has a classic fit and only the
-                most essential details.{' '}
+                Made in Holland, Uk and USA, these batteries offer the best there is to offer..{' '}
               </p>
               <p>
-                With over 100 years spent perfecting fabric, fit and style, the
-                Sunspel Classic T-shirt is recognised as the finest in the
-                world.
+                The Very Best Testing.
               </p>
             </div>
           </div>
